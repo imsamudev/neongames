465646d89f401c15047c10f03994ccfc4cc37208
Samuel Figueroa <samuel96figueroa@gmail.com>
2024-04-26 18:06:41 -0300

    barra-navegacion_fondo-añadido

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-import './style.css'
+import "./style.css";
 
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const navbarLinks = document.getElementById("navbar-links");
+
+hamburgerMenu.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
