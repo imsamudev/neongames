c4a5033036cf1b71cca66078f7ce23061b0031f3
Samuel Figueroa <160678978+imsamudev01@users.noreply.github.com>
2024-05-06 12:48:58 -0300

    Revert "Subida de archivos webps para cards"

--- main.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.getElementById("hamburger-menu");
-const navbarLinks = document.getElementById("navbar-links");
-
-hamburgerMenu.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
